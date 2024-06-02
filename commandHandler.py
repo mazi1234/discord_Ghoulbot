7c48836337dadc7b2b482e1226fc40d9baae1784
mazi1234 <98608480+mazi1234@users.noreply.github.com>
2024-06-02 14:36:13 +0300

    added documentation

--- commandHandler.py
+++ commandHandler.py
@@ -15,32 +15,32 @@ async def show(ctx):
 @bot.group(invoke_without_command=True)
 async def set(ctx):
     """_summary_
-
+    Handles any set commands that are not explicitly handled in their own functions
     Args:
-        ctx (_type_): _description_
+        ctx (discord.ext.commands.Context): The context this command was called under
     """
     await ctx.send('command {ctx} not found')
 @set.command(name=cmds.get("set_min_users"))
-async def set_min_users(ctx, arg=None):
+async def set_min_users(ctx, arg:int =None):
     """_summary_
-
+    Sets the minimum number of users that must join before a notification is sent
     Args:
-        ctx (_type_): _description_
-        arg (_type_, optional): _description_. Defaults to None.
+        ctx (discord.ext.commands.Context): The context this command was called under
+        arg (int, optional): The new value for the minimum number of users . Defaults to None.
     """
-    if (await handleMissingArg(ctx, arg)):
+    if (await isValidArg(ctx, arg)):
         old = cmds.get("set_min_users")
         envConfig.set(cmds.get("set_min_users"), arg)
         logger.info(f'Successfully configured the {cmds.get("set_min_users")} to {arg} from {old}')
 @set.command(name=cmds.get("set_max_users"))
-async def set_max_users(ctx, arg=None):
+async def set_max_users(ctx, arg:int=None):
     """_summary_
         Changes the maximum number of users the bot will send join notifications for. After the maximum users have joined, the bot will not send any more notifications to the text channel
     Args:
-        ctx (_type_): _description_
-        arg (_type_, optional): _description_. Defaults to None.
+        ctx (discord.ext.commands.Context): The context this command was called under
+        arg (int, optional): The new value for the maximum number of users. Defaults to None.
     """
-    if (await handleMissingArg(ctx,arg)):
+    if (await isValidArg(ctx,arg)):
         old = cmds.get("set_max_users")
         envConfig.set(cmds.get("set_max_users"), arg)
         logger.info(f'Successfully configured the {cmds.get("set_max_users")} to {arg} from {old}')
@@ -53,9 +53,19 @@ async def on_ready():
     """
     print(f'{bot.user} has connected to Discord!')
     print(bot.guilds)
-async def handleMissingArg(ctx, arg) -> bool:
-    if arg == None: 
+async def isValidArg(ctx, arg) -> bool:
+    """_summary_
+    Checks if the argument is valid or missing. If missing it will alert the user in discord (by sending a message) and also logging the failure.
+    Args:
+        ctx (discord.ext.commands.Context): the context of the command.
+        arg (any): the argument to check if missing or not
+
+    Returns:
+        bool: True if the argument is valid. False if missing
+    """
+    if arg is None: 
         await ctx.send(f'Invalid Command. Error: Missing argument. Use {cmd_prefix}help to print the manual')
+        logger.info(f'There\'s a missing argument. Context {ctx}')
         return False
     return True
 bot.run(TOKEN)
